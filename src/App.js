780d602e6d1b38be8e41bc35f80b086a3ecdfec1
Sophie-Zulinski <sophie.zulinski@gmx.at>
2023-01-23 15:24:46 +0100

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import logo from './logo.svg';
-import './App.css';
+
+const bannerSectionStyles = css`
+  background-color: transparent;
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+
+  padding: 16px;
+  a {
+    color: #e2e8f0;
+    text-decoration: none;
+    font-size: 1.125rem;
+  }
+`;
+
+const topBottonStyles = css`
+  align-items: center;
+  background: rgb(74, 144, 226);
+  border-radius: 100px;
+  border: none;
+  box-shadow: none;
+  color: rgb(255, 255, 255);
+  display: inline-flex;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 28px;
+  justify-content: center;
+  cursor: pointer;
+  outline: none;
+  min-height: 48px;
+  min-width: 48px;
+  padding: 0.75rem 1.11111rem 0.75rem 1.24074rem;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <section css={bannerSectionStyles}>
+          <div>
+            <a href="/#">
+              <img
+                src="https://www.calm.com/_n/images/calm-logo-gradient.svg"
+                alt="calm logo"
+              />
+            </a>
+          </div>
+          <div css={topBottonStyles}>Purchase</div>
+        </section>
       </header>
-    </div>
+
+      <body>body</body>
+      <footer>footer</footer>
+    </>
   );
 }
 
